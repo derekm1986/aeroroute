a3bc30650afc24a1e222a537cd976a00469830be
derekm1986 <derekm1986@gmail.com>
2016-09-05 22:52:16 -0400

    tiebreaker functionality improvements

--- tiebreaker.py
+++ tiebreaker.py
@@ -2,25 +2,44 @@
 
 def tiebreaker(inputwaypoints):
 
-    shortestdistance = 999999.0 #establish worst case scenario so anything would be better
+    shortestdistance = 1000000000.0 #establish worst case scenario so anything would be better
     #shortestdistanceset = #combination of waypoints with shortest distance 
     
     foundmultiples = [i for i,x in enumerate(inputwaypoints) if type(x[2]) is list]
     
-    print foundmultiples
+    #how many waypoints with multiples?
+    print "Number of total waypoints with multiples:",len(foundmultiples)
+    
+    print "Found multiples at position(s):",foundmultiples
+    
+    possibilitymatrix = []  #fill with possiblities to try in waypointnumber, latlongnumber format
+    
+    #how to figure out how many "groups" of multiples there are - are items in foundmultiples consecutive?
+    #are these waypoints congruous or are there multiple sets?
+    
+    position = 0
     
     for waypoints in inputwaypoints:
         if type(waypoints[2]) is list: #multiple lat/long tuples were found in a list
 
     ###################################        
-            print "Multiple items were found with name", waypoints[0], "...need more programming.  Without further programming, first lat/long will be used."
+            print "Multiple items were found with name", waypoints[0], "...need more programming."  
+            print "Number of", waypoints[0], "lat/long possibilties:", len(waypoints[2])
+            
+            possibilitymatrix.append((position,(range(len(waypoints[2])))))
+            
             waypoints[2] = waypoints[2][0] #remove these when you make logic to do something with multiple lat/longs
+            print "Without further programming, first lat/long will be used." #remove these when you make logic to do something with multiple lat/longs
+            
+            position = position + 1
+            
+        else:
+            position = position + 1
     ###################################
     
-    #how many waypoints with multiples?
-    print len(foundmultiples)
-    
-    #are these waypoints congruous or are there multiple sets?
+
+    print possibilitymatrix
+
 
     return inputwaypoints
 
