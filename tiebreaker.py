7221b15b9865662bf89479208973a178e1401793
unknown <derekm1986@gmail.com>
2018-03-11 19:51:42 -0400

    tiebreaker in middle not working

--- tiebreaker.py
+++ tiebreaker.py
@@ -34,90 +34,86 @@ def tiebreaker(inputwaypoints):
                 print('Single multiple was found at the beginning')
                 shortestdistance = float("inf")  # establish worst case scenario so anything would be better
                 possibility = 0
-                for iter in inputwaypoints[0][2]:
-                    tryset = [[inputwaypoints[0][2][possibility], inputwaypoints[1][2][0]]]
-                    trydistance = vincenty.vincenty(inputwaypoints[0][2][possibility], inputwaypoints[1][2][0])
+                for iter in inputwaypoints[0].getpossibilities():
+                    tryset = [inputwaypoints[0].getpossibilities()[possibility], inputwaypoints[1].getcoordinates()]
+                    trydistance = vincenty.vincenty(inputwaypoints[0].getpossibilities()[possibility].getcoordinates(), inputwaypoints[1].getcoordinates())
                     if trydistance < shortestdistance:
                         shortestdistance = trydistance
                         shortestpossibility = possibility
                         shortestset = tryset                    
                     possibility += 1
                     # put lat/long back where it belongs
-                inputwaypoints[0][2] = [inputwaypoints[0][2][shortestpossibility]]
+                inputwaypoints[0] = inputwaypoints[0].getpossibilities()[shortestpossibility]
             elif len(inputwaypoints) - 1 in multipleset:
                 print('Single multiple was found at the end')
                 shortestdistance = float("inf")  # establish worst case scenario so anything would be better
                 possibility = 0
-                for iter in inputwaypoints[multipleset[0]][2]:
-                    tryset = [[inputwaypoints[multipleset[0]-1][2][0], inputwaypoints[multipleset[0]][2][possibility]]]
-                    trydistance = vincenty.vincenty(inputwaypoints[multipleset[0]-1][2][0], inputwaypoints[multipleset[0]][2][possibility])
+                for iter in inputwaypoints[multipleset[0]].getpossibilities():
+                    tryset = [inputwaypoints[multipleset[0]-1].getcoordinates(), inputwaypoints[multipleset[0]].getpossibilities()[possibility]]
+                    trydistance = vincenty.vincenty(inputwaypoints[multipleset[0]-1].getcoordinates(), inputwaypoints[multipleset[0]].getpossibilities()[possibility].getcoordinates())
                     if trydistance < shortestdistance:
                         shortestdistance = trydistance
                         shortestpossibility = possibility
                         shortestset = tryset
                     possibility += 1
                     # put lat/long back where it belongs
-                inputwaypoints[multipleset[0]][2] = [inputwaypoints[multipleset[0]][2][shortestpossibility]]
+                inputwaypoints[multipleset[0]] = inputwaypoints[multipleset[0]].getpossibilities()[shortestpossibility]
             else:
                 print('Single multiple found in middle of route')
                 shortestdistance = float("inf")  # establish worst case scenario so anything would be better
                 possibility = 0
-                for iter in inputwaypoints[multipleset[0]][2]:
-                    tryset = [[inputwaypoints[multipleset[0]-1][2][0], inputwaypoints[multipleset[0]][2][possibility], inputwaypoints[multipleset[0]+1][2][0]]]
-                    trydistance = vincenty.vincenty(inputwaypoints[multipleset[0]-1][2][0], inputwaypoints[multipleset[0]][2][possibility]) + vincenty.vincenty(inputwaypoints[multipleset[0]][2][possibility], inputwaypoints[multipleset[0]+1][2][0])
-                    if trydistance < shortestdistance:
-                        shortestdistance = trydistance
-                        shortestpossibility = possibility
-                        shortestset = tryset
+                for iter in inputwaypoints[multipleset[0]].getpossibilities():
+                    tryset = [inputwaypoints[multipleset[0]-1].getcoordinates(), inputwaypoints[multipleset[0]].getpossibilities()[possibility], inputwaypoints[multipleset[0]+1].getcoordinates]
+                    trydistance = vincenty.vincenty(inputwaypoints[multipleset[0]-1].getcoordinates(), inputwaypoints[multipleset[0]].getpossibilities()[possibility].getcoordinates()) + vincenty.vincenty(inputwaypoints[multipleset[0]].getpossibilities()[possibility].getcoordinates())
                     possibility += 1
                     # put lat/long back where it belongs
-                inputwaypoints[multipleset[0]][2] = [inputwaypoints[multipleset[0]][2][shortestpossibility]]
-        elif len(multipleset) > 1:  # more than one multiple is inside multipleset
-            print('multiple set with more than one multiple found, no functionality yet')
-            if 0 in multipleset and len(inputwaypoints) - 1 in multipleset:  # all are multiples
-                print('all are multiples')
-                possibilitytree = []
-                # for possibility in multipleset:
-            elif 0 in multipleset:  # starts at beginning of route
-                print('starts at beginning')
-                possibilitytree = []
-                # for possibility in multipleset:
-                # use end + 1 append to all
-            elif len(inputwaypoints) - 1 in multipleset:  # ends at end of route
-                print('ends at end')
-                # use beginning - 1
-                possibilitytree = [inputwaypoints[multipleset[0]-1][2][0]]
-                
-                iterator = 0
-
-                for possibility in multipleset:
+                inputwaypoints[multipleset[0]] = inputwaypoints[multipleset[0]].getpossibilities()[shortestpossibility]
+#        elif len(multipleset) > 1:  # more than one multiple is inside multipleset
+#            print('multiple set with more than one multiple found, no functionality yet')
+#            if 0 in multipleset and len(inputwaypoints) - 1 in multipleset:  # all are multiples
+#                print('all are multiples')
+#                possibilitytree = []
+#                # for possibility in multipleset:
+#            elif 0 in multipleset:  # starts at beginning of route
+#                print('starts at beginning')
+#                possibilitytree = []
+#                # for possibility in multipleset:
+#                # use end + 1 append to all
+#            elif len(inputwaypoints) - 1 in multipleset:  # ends at end of route
+#                print('ends at end')
+#                # use beginning - 1
+#                possibilitytree = [inputwaypoints[multipleset[0]-1][2][0]]
+#
+#                iterator = 0
+#
+#                for possibility in multipleset:
                     
-                    position = 0
+#                    position = 0
 
-                    possibilitytree.append(possibilitytree[0])
+#                    possibilitytree.append(possibilitytree[0])
 
-                    for latlong in inputwaypoints[possibility][2]:
+#                    for latlong in inputwaypoints[possibility][2]:
 
-                        possibilitytree.append(inputwaypoints[possibility][2][position])
+#                        possibilitytree.append(inputwaypoints[possibility][2][position])
                     
-                        position += 1
+#                        position += 1
 
-                    iterator += 1
+#                    iterator += 1
 
                 # for possibility in multipleset:
                     # copy and extend/append to possibilitytree
-                print(possibilitytree)            
-            else: # in middle of route
-                print('in middle')
-                possibilitytree = []
+#                print(possibilitytree)
+#            else: # in middle of route
+#                print('in middle')
+#                possibilitytree = []
                 # use beginning - 1
                 # for possibility in mutlipleset:
                 # use end + 1 append to all
 
-            for waypoint in multipleset:
-                print('multipleset contains',waypoint, 'length', len(inputwaypoints[waypoint][2]))
-                for possibility in inputwaypoints[waypoint][2]:
-                    print(possibility)
+#            for waypoint in multipleset:
+#                print('multipleset contains',waypoint, 'length', len(inputwaypoints[waypoint][2]))
+#                for possibility in inputwaypoints[waypoint][2]:
+#                    print(possibility)
                     
 
             
