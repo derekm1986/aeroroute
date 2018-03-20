ef37dd0a89239be10e7baa65fc4399de6dcc30aa
derekm1986 <derekm1986@users.noreply.github.com>
2018-03-19 21:00:24 -0400

    brought over pairmaker

--- functions.py
+++ functions.py
@@ -1,11 +1,22 @@
 import vincenty
 import pairmaker
 
+
+def pairmaker(inputwaypoints):
+
+    i = 0
+
+    while i <= (len(inputwaypoints) - 2):  # make pairs of each waypoint and the waypoint after it
+        pair = [inputwaypoints[i], inputwaypoints[i + 1]]
+        i += 1
+        yield pair
+
+
 def distancefinder(input):
   
   sumdistance = 0.00  # establish sumdistance and put zero in it
   
-  for pair in pairmaker.pairmaker(input):
+  for pair in pairmaker(input):
     pairdistance = vincenty.vincenty(*pair)
     sumdistance += pairdistance
     
