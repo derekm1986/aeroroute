89e16c04a2070b467019929554f4a49d05d65af1
unknown <derekm1986@gmail.com>
2018-03-19 20:15:29 -0400

    fix distancefinder

--- functions.py
+++ functions.py
@@ -6,7 +6,7 @@ def distancefinder(input):
   sumdistance = 0.00  # establish sumdistance and put zero in it
   
   for pair in pairmaker.pairmaker(input):
-    pairdistance = vincenty.vincenty(pair) # may need a *
+    pairdistance = vincenty.vincenty(*pair)
     sumdistance += pairdistance
     
   return sumdistance
