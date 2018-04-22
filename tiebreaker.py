28021f4914688f7d377f552609ce01928383d33c
derekm1986 <derekm1986@users.noreply.github.com>
2018-04-21 21:29:23 -0400

    no more tryset

--- tiebreaker.py
+++ tiebreaker.py
@@ -16,13 +16,11 @@ def tiebreaker(inputwaypoints, multiplesmatrix):
                 shortestdistance = float("inf")  # establish worst case scenario so anything would be better
                 possibility = 0
                 for iter in inputwaypoints.getpossibilities(0):
-                    tryset = [inputwaypoints.getroute()[0].getpossibilities()[possibility], inputwaypoints.getroute()[1]]
                     trydistance = functions.vincentyindirect((inputwaypoints.getroute()[0].getpossibilities()[possibility],
                         inputwaypoints.getroute()[1]))
                     if trydistance < shortestdistance:
                         shortestdistance = trydistance
                         shortestpossibility = possibility
-                        shortestset = tryset
                     possibility += 1
                     # put lat/long back where it belongs
                 inputwaypoints.deambiguate(0, shortestpossibility)
@@ -31,14 +29,11 @@ def tiebreaker(inputwaypoints, multiplesmatrix):
                 shortestdistance = float("inf")  # establish worst case scenario so anything would be better
                 possibility = 0
                 for iter in inputwaypoints.getpossibilities(multipleset[0]):
-                    tryset = [inputwaypoints.getroute()[multipleset[0]-1],
-                        inputwaypoints.getroute()[multipleset[0]].getpossibilities()[possibility]]
                     trydistance = functions.vincentyindirect((inputwaypoints.getroute()[multipleset[0]-1],
                         inputwaypoints.getroute()[multipleset[0]].getpossibilities()[possibility]))
                     if trydistance < shortestdistance:
                         shortestdistance = trydistance
                         shortestpossibility = possibility
-                        shortestset = tryset
                     possibility += 1
                     # put lat/long back where it belongs
                 inputwaypoints.deambiguate(multipleset[0], shortestpossibility)
@@ -47,9 +42,6 @@ def tiebreaker(inputwaypoints, multiplesmatrix):
                 shortestdistance = float("inf")  # establish worst case scenario so anything would be better
                 possibility = 0
                 for iter in inputwaypoints.getpossibilities(multipleset[0]):
-                    tryset = [inputwaypoints.getroute()[multipleset[0]-1],
-                        inputwaypoints.getroute()[multipleset[0]].getpossibilities()[possibility],
-                        inputwaypoints.getroute()[multipleset[0]+1]]
                     trydistance = functions.vincentyindirect((inputwaypoints.getroute()[multipleset[0]-1],
                         inputwaypoints.getroute()[multipleset[0]].getpossibilities()[possibility])) + \
                         functions.vincentyindirect((inputwaypoints.getroute()[multipleset[0]].getpossibilities()[possibility],
@@ -57,7 +49,6 @@ def tiebreaker(inputwaypoints, multiplesmatrix):
                     if trydistance < shortestdistance:
                         shortestdistance = trydistance
                         shortestpossibility = possibility
-                        shortestset = tryset
                     possibility += 1
                     # put lat/long back where it belongs
                 inputwaypoints.deambiguate(multipleset[0], shortestpossibility)
