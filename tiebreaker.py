efe0641e4eaba589e755cd4983b844774cca5ac6
derekm1986 <derekm1986@users.noreply.github.com>
2018-05-05 17:48:55 -0400

    readability

--- tiebreaker.py
+++ tiebreaker.py
@@ -67,7 +67,8 @@ def testtiebreaker(inputwaypoints, multiplesmatrix):
         print('multipleset was: ', multipleset)
 
         for listposition in multipleset:
-            multiplesetelements.append(objects.TBWrapper(inputwaypoints.getelement(listposition), listposition, True))
+            multiplesetelements.append(objects.TBWrapper(inputwaypoints.getelement(listposition), 
+                                       listposition, True))
         
         allareambiguous = False
         firstisambiguous = False
@@ -85,12 +86,12 @@ def testtiebreaker(inputwaypoints, multiplesmatrix):
         if allareambiguous is False and firstisambiguous is False:
             # add previous waypoint to beginning of testmultipleset
             multiplesetelements.insert(0, objects.TBWrapper(inputwaypoints.getelement(multipleset[0] - 1),
-                                                             multipleset[0] - 1))
+                                       multipleset[0] - 1))
             
         if allareambiguous is False and lastisambiguous is False:
             # add following waypoint to end of testmultipleset 
             multiplesetelements.append(objects.TBWrapper(inputwaypoints.getelement(multipleset[-1] + 1),
-                                                           multipleset[-1] + 1))
+                                       multipleset[-1] + 1))
             
 
         print('multiplesetelements is: ', multiplesetelements)
