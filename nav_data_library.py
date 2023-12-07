3293aec36fb116af02c79d7b583e961ca3705087
derekm1986 <derekm1986@users.noreply.github.com>
2023-12-07 09:02:17 -0500

    Update nav_data_library.py

--- nav_data_library.py
+++ nav_data_library.py
@@ -7,21 +7,29 @@ import atsreader
 class NavDataLibrary(object):
     # trying to keep all the nav data in an object
     def __init__(self):
-        print('   OBJLoading airports into memory...', end="")
+        print('   OBJ*Loading airports into memory...', end="")
         self.airport_dict = airportsreader.airport_dict_maker()
         print('OK')  # loading airports was successful
 
-        print('   OBJLoading NAVAIDs into memory...', end="")
+        print('   OBJ*Loading NAVAIDs into memory...', end="")
         self.navaid_dict = navaidsreader.navaid_dict_maker()
         print('OK')  # loading NAVAIDs was successful
 
-        print('   OBJLoading waypoints into memory...', end="")
+        print('   OBJ*Loading waypoints into memory...', end="")
         self.waypoint_dict = waypointsreader.waypoint_dict_maker()
         print('OK')  # loading elements was successful
 
-        print('   OBJLoading airways into memory...', end="")
+        print('   OBJ*Loading airways into memory...', end="")
         self.airway_dict = atsreader.airway_dict_maker()
         print('OK')  # loading airways was successful
+
+        print('\nOBJ*Combining NAVAID and waypoint dictionaries...', end="")
+        self.points_in_space_dict = functions.points_in_space_dict_combiner(self.navaid_dict, self.waypoint_dict)
+        print("OK")  # dictionary combination was successful
+        
+        print('OBJ*Adding airway references to combined dictionary...', end="")
+        self.points_in_space_dict = airwaymatcher.airway_matcher(self.points_in_space_dict, self.airway_dict)
+        print("OK")  # reference matching was successful
 
     def add_airport_dict(self, airport_dict):
         self.airport_dict = airport_dict
