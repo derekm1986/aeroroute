26f678e2d9ed807850a4252459dbf6a54aa2d54c
derekm1986 <derekm1986@users.noreply.github.com>
2023-12-21 09:51:49 -0500

    Update objects.py

--- objects.py
+++ objects.py
@@ -244,7 +244,7 @@ class AmbiguousAirway:
 
 
 class Coordinates:
-    # store coordinates here
+    # store coordinates here - validation checker may have to exist outside of this class - how could an instance return itself as none?
     def __init__(self, latitude: str, longitude: str):
         self._latitude: str
         self._longitude: str
