446f959b15dbdfc6fc92606dd271c1d42b144823
derekm1986 <derekm1986@users.noreply.github.com>
2024-03-19 07:04:25 -0400

    renamed multiple_finder

--- aeroroute.py
+++ aeroroute.py
@@ -104,7 +104,7 @@ def main() -> None:
         if input_route_obj.contains_ambiguous_point:  # adjacent airways didn't find everything, brute is needed
 
             logging.info("Ambiguous point(s) still detected. Using brute deambiguator.")
-            multiples_matrix = functions.multiple_finder(input_route_obj)
+            multiples_matrix = functions.multiple_point_finder(input_route_obj)
             input_route_obj = functions.deambiguator_brute(input_route_obj, multiples_matrix)
 
         for item in input_route_obj.elements:
