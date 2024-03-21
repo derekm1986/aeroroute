2720740d2989aa1280e9ec5d7795a18ed9c3f752
derekm1986 <derekm1986@users.noreply.github.com>
2024-03-21 14:11:57 -0400

    trying to get both working

--- app.py
+++ app.py
@@ -3,48 +3,32 @@ from aeroroute import aeroroute_input
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     # Call the main function from aeroroute.py
+@app.route('/distance', methods=['GET', 'POST'])
+def distancefinder():
     
-#     query = ("KJFK", "EGLL")
-
-#     result = aeroroute_input((query))
-
-#     # Return the result as a response
-#     #return str(result)
-    
-#     # Render the index.html template and pass the result to it
-#     return render_template('index.html', query=query, result=result)
-    
-    
-#     #return f"Result from aeroroute.py: {result}"
-
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'POST':
-#         route = request.form.get('route').upper()
-#         query = (route.split())
-#         result = aeroroute_input(query)
-#         return render_template('result.html', route=route, result=result)
-#     return render_template('form.html')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-from flask import Flask, render_template, request
-from aeroroute import aeroroute_input
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
     route = result = None
+    
     if request.method == 'POST':
         route = request.form.get('route').upper()
         query = route.split()
         result = aeroroute_input(query)
+    
     return render_template('form.html', route=route, result=result)
+
+@app.route('/cdr', methods=['GET', 'POST'])
+def cdr():
+    
+    result = None
+    departure = str()
+    arrival = str()
+
+    if request.method == 'POST':
+        departure = request.form.get('departure')
+        arrival = request.form.get('arrival')
+        print(departure, arrival)
+        result = ("place holder", departure, arrival)
+    
+    return render_template('cdr.html', departure=departure, arrival=arrival, result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
