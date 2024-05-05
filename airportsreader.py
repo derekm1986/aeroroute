1d6c1906d1e0afd2775207b6a1bd9c28fcb4888e
derekm1986 <derekm1986@users.noreply.github.com>
2024-05-05 19:33:51 -0400

    trying to get back

---
+++ airportsreader.py
@@ -0,0 +1,63 @@
+# This file parses the vasFMC format Airports.txt file
+# Airports.txt file must be in AIRAC directory
+
+# This file does not attempt to read runway data
+
+from objects import Airport
+from objects import Coordinates
+
+#from aeroroute.objects import Airport
+#from aeroroute.objects import Coordinates
+
+
+def airport_dict_maker():
+
+    airport_file = open("data/AIRAC/Airports.txt")
+
+    airport_dict = {}
+
+    for line in airport_file:
+        if line.startswith("A"):
+            currentline = line.rstrip().split("|")
+            airportid = currentline[1]
+            airportname = currentline[2]
+            airportlat = currentline[3]
+            airportlong = currentline[4]
+
+            airportlatisnegative = False  # establish variable
+
+            if airportlat.startswith("-"):
+                airportlatisnegative = True
+                airportlat = airportlat[1:]
+
+            if len(airportlat) < 7:
+                airportlat = "0" * (7 - len(airportlat)) + airportlat
+
+            airportlatwithdecimal = airportlat[:-6] + "." + airportlat[-6:]  # 6 decimal places
+
+            if airportlatisnegative is True:
+                airportlatwithdecimal = "-" + airportlatwithdecimal
+
+            airportlongisnegative = False  # establish variable
+
+            if airportlong.startswith("-"):
+                airportlongisnegative = True
+                airportlong = airportlong[1:]
+
+            if len(airportlong) < 7:
+                airportlong = "0" * (7 - len(airportlong)) + airportlong
+
+            airportlongwithdecimal = airportlong[:-6] + "." + airportlong[-6:]  # 6 decimal places
+
+            if airportlongisnegative is True:
+                airportlongwithdecimal = "-" + airportlongwithdecimal
+
+            airportcoordinates = Coordinates(airportlatwithdecimal, airportlongwithdecimal)
+
+            airportobj = Airport(airportid, airportcoordinates, airportname)
+
+            airport_dict[airportid] = airportobj
+
+    airport_file.close()
+
+    return airport_dict
---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+#from aeroroute import aeroroute_input
+#from cdrgetter import cdr_finder_dep_arr
+
+from aeroroute.aeroroute import aeroroute_input
+from aeroroute.cdrgetter import cdr_finder_dep_arr
+
+app = Flask(__name__)
+
+@app.route('/distance', methods=['GET', 'POST'])
+def distancefinder():
+    
+    route = result = None
+    
+    if request.method == 'POST':
+        route = request.form.get('route').upper()
+        result = aeroroute_input(route)
+    
+    return render_template('distance.html', route=route, result=result)
+
+@app.route('/cdr', methods=['GET', 'POST'])
+def cdr():
+    
+    result = None
+    departure = str()
+    arrival = str()
+
+    if request.method == 'POST':
+        result = None
+        departure = request.form.get('departure').upper()
+        arrival = request.form.get('arrival').upper()
+        result = cdr_finder_dep_arr(departure, arrival)
+    
+    return render_template('cdr.html', departure=departure, arrival=arrival, result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
