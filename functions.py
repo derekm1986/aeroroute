b721adf493b1043665a0e32902a9aebbb040428b
derekm1986 <derekm1986@users.noreply.github.com>
2025-03-06 09:48:11 -0500

    removing airway from beginning or end

--- functions.py
+++ functions.py
@@ -206,16 +206,14 @@ def multiple_types_resolver(input_route: objects.Route) -> objects.Route:
         for item in input_route.first_element:
             if type(item) == objects.Airway or type(item) == objects.AmbiguousAirway:
                 print("airway detected at beginning")
-                print(item)  # we still need to remove this out of the list, use delete_element_from_list
-
+                input_route.delete_element_from_list(0, input_route.first_element.index(item))
 
     # if input_route.last_element contains an airway, remove it!
     if type(input_route.last_element) == list:
         for item in input_route.last_element:
             if type(item) == objects.Airway or type(item) == objects.AmbiguousAirway:
                 print("airway detected at end")
-                print(item)  #  we still need to remove this out of the list, use delete_element_from_list
-
+                input_route.delete_element_from_list(-1, input_route.last_element.index(item))
 
     # favor airports in first or last positions
 
