f3e22c631f935a0fef4db916d48a77cc348bb010
derekm1986 <derekm1986@users.noreply.github.com>
2025-03-25 09:42:43 -0400

    working on string representation

--- objects.py
+++ objects.py
@@ -402,3 +402,7 @@ class NavDictEntry:
 
     def add_airway(self, airway) -> None:
         self._airways = airway
+
+    def __repr__(self):
+        return self._identifier + ' ' + str(self._airports) + ' ' + str(self._points_in_space) + ' ' + str(self._airways)
+    
