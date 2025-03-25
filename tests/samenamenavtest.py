119975c05c21ae40eb474a7a28e91e4addb567ca
derekm1986 <derekm1986@users.noreply.github.com>
2025-03-25 10:16:04 -0400

    new file structure

---
+++ tests/samenamenavtest.py
@@ -0,0 +1,6 @@
+from .. import nav_data_library as ndl
+
+# this file is just to test imports so I can put tests in a separate directory
+
+# now go through all nav data libraries and look for navaids, airports, airways with the same names
+
