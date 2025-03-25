639ba17c8d4efcd4816ded358716042e73b2a4c9
derekm1986 <derekm1986@users.noreply.github.com>
2025-03-25 12:51:58 -0400

    Update srctestmoduletest.py

--- tests/srctestmoduletest.py
+++ tests/srctestmoduletest.py
@@ -4,9 +4,12 @@ import os
 # Add the src directory to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-import testmodule as tm
+import testmodule as tm # type: ignore
 
 print("success")
+
+tm.testmessage()
+
 # this file is just to test imports so I can put tests in a separate directory
 
 # now go through all nav data libraries and look for navaids, airports, airways with the same names
