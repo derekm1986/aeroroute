0bb93b3943fa69e06e86a9bca8fb3d85a6b2251c
derekm1986 <derekm1986@users.noreply.github.com>
2025-03-25 12:36:18 -0400

    got 1 working test

--- tests/samenamenavtest.py
+++ tests/samenamenavtest.py
@@ -1,5 +1,12 @@
-from .. import nav_data_library as ndl
+import sys
+import os
 
+# Add the parent directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import nav_data_library as ndl
+
+print("success")
 # this file is just to test imports so I can put tests in a separate directory
 
 # now go through all nav data libraries and look for navaids, airports, airways with the same names
