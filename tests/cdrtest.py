22df5dc95bee72ca8b6214dcca2546ab09d8e214
derekm1986 <derekm1986@users.noreply.github.com>
2025-03-26 09:57:50 -0400

    remove unnecessary import statement

--- tests/cdrtest.py
+++ tests/cdrtest.py
@@ -4,7 +4,6 @@ import os
 # Add the src directory to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-import aeroroute # type: ignore
 import cdrreader # type: ignore
 
 # test aeroroute using all cdrs
@@ -12,5 +11,4 @@ import cdrreader # type: ignore
 cdr_dict = cdrreader.cdr_dict_maker()
 
 for val in cdr_dict.values():
-    #print(aeroroute.aeroroute_input(val.route_string))
     print(val.cdr_id, val.distance)
